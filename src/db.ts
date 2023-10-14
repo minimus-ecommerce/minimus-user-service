e386de6f6406285a0cbc3f5437b9cf4c56f12bff
Akash NP <akashnp1998@gmail.com>
2023-10-14 16:04:22 +0400

    chore: use host based on env, ensures correct host name in container

--- src/db.ts
+++ src/db.ts
@@ -2,6 +2,7 @@ import pg from 'pg';
 
 const { Client } = pg;
 export const db = new Client({
+    host: process.env["DB_HOST"],
     database: process.env["DB_NAME"],
     user: process.env["DB_USER"],
     password: process.env["DB_PASSWORD"],
